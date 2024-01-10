98fdc7da69cfd5fc88917d3f7c9da46aed6a2007
danaxsax <dana.salinas.tareas@gmail.com>
2024-01-09 22:31:04 -0600

    utlimos cambios

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,9 @@
-# Crear modelos (tablas de sql) con palabra reservada classs mediante migraciones 
+# Crear modelos (tablas de sql) con palabra reservada class mediante migraciones 
 from django.db import models
 
 class Project(models.Model): #nombre de la clase (herencia)
                 #CharField - texto
     name = models.CharField(max_length= 30) #atributo = modelo.tipo de dato (longitud)
-    
     #configuracion para mostrar nombre en el admin
     def __str__(self) -> str:
         return self.name
@@ -13,7 +12,7 @@ class Project(models.Model): #nombre de la clase (herencia)
 class Task(models.Model):
     title = models.CharField(max_length=40)
     description = models.CharField(max_length = 400)
-    #               claveforanea(class q pertenece la key. on_delete=encaso de bottar el project, las task relacionadas se eliminan tmbn )
+    #               claveforanea(class q pertenece la key. on_delete=en caso de bottar el project, las task relacionadas se eliminan tmbn )
     project = models.ForeignKey(Project, on_delete=models.CASCADE) 
     done = models.BooleanField(default=False)
     #configuracion para mostrar nombre en el admin
